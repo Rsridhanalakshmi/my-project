14887e03d8b79635693e12c409f0ff35487b7832
Rsridhanalakshmi <rsridhanalakshmi@gmail.com>
2026-07-21 14:36:20 +0530

    Add Netlify redirects

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://gconnect-dev.gdinexus.com:8409',
+      "/api": {
+        target: "https://gconnect-dev.gdinexus.com:8409",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
